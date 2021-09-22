a7e5f4f7aa7e96bec2ac47dfe4481c06a86218a4
LarrocaX <86024705+LarrocaX@users.noreply.github.com>
2021-09-22 12:08:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const bdark = document.querySelector('#bdark');
+const body = document.querySelector('body');
+
+txtdark();
+load();
+
+bdark.addEventListener('click', e =>{
+    body.classList.toggle('dark');
+    store(body.classList.contains('dark'));
+    txtdark();
+
+});
+function load(){
+    const dark = localStorage.getItem('dark');
+
+    if(!dark){
+         store('false')
+        }    else if(dark == 'true'){
+
+            body.classList.add('dark');
+    }
+}
+function store(value){
+    localStorage.setItem('dark', value)
+    
+}
+function txtdark(){
+    if(body.classList.contains('dark')){
+
+    document.getElementById("bdark").innerHTML = "LIGHT MODE";
+}
+    else {
+    document.getElementById("bdark").innerHTML = "DARK MODE";
+}
+}
